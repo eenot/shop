f3ba6498c3a3e93cf7fd7304c097933b41d7bc34
Thomas Weiser <git@thomasweiser.de>
2016-03-31 22:01:56 +0200

    Use firebase-queue to get requests from client.
    This way we can safely run more than one server process consuming requests.
    
    See
    - https://github.com/firebase/firebase-queue
    - https://www.firebase.com/blog/2015-05-15-introducing-firebase-queue.html

--- server/lib/index.js
+++ server/lib/index.js
@@ -1,8 +1,10 @@
 
 var Firebase = require("firebase");
+var Queue = require('firebase-queue');
 
 var root = new Firebase("https://plentifulshop-demo.firebaseio.com/");
 
+// TODO: Example code, to be removed later.
 root
   .child ("shop/name")
   .once ("value",
@@ -13,4 +15,25 @@ root
       console .error (err.message);
     }
   );
+
+// TODO: Define security rules for the queue and grant access for the server
+
+var queue =
+  new Queue (
+    root .child ("purchases/queue"),
+    function (data, progress, resolve, reject) {
+
+      console.log(data);
+
+      // Do some work
+      progress(50);
+
+      // Finish the task asynchronously
+      setTimeout(function() {
+        resolve();
+      }, 1000);
+
+    }
+  );
+
 
